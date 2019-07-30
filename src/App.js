408e6d827bc62c0ea26f677456bca80a8d0d2af1
tschroedertlc <tschroeder@thelarkincompany.com>
2019-07-30 12:19:06 -0700

    Changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my humble abode
+          Changed, am I?
         </p>
         <a
           className="App-link"
